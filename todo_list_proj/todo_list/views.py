d3d4300f7639c88478888360b1ba07be84c35129
seoulsky-field <seoulsky.field02@gmail.com>
2024-04-22 11:18:08 +0000

    Chore: delete comments which codes are related to templates (Agile Stage 2)

--- todo_list_proj/todo_list/views.py
+++ todo_list_proj/todo_list/views.py
@@ -1,8 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-# from .models import TodoList
-
-# def todo_list(request):
-#     todo_all = TodoList.objects.all()
-#     return render(request, 'todo_list/main_view.html', {'todo_list': todo_all})
